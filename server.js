a6c274680d8100f6d772ce01039d1686bf26cfa6
Azam Awan <admin@Azams-MacBook-Pro.local>
2020-03-05 10:38:39 +0100

    Semantiek code toegevoegd

--- server.js
+++ server.js
@@ -17,37 +17,59 @@ app.use(express.static('views'))
 
 // index page
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index', {
+      title: 'Lovr - Home',
+      page_name: 'Home'
+  });
+    
 });
 
 // index page
 app.get('/index', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {
+    title: 'Lovr - Home',
+    page_name: 'home'
+});
 });
 
 // login page
 app.get('/login', function(req, res){
-  res.render('pages/login');
+  res.render('pages/login', {
+    title: 'Lovr - Login',
+    page_name: 'login'
+});
 });
 
 // over page
 app.get('/over', function(req, res) {
-    res.render('pages/over');
+    res.render('pages/over', {
+      title: 'Lovr - Over',
+      page_name: 'over'
+  });
 });
 
 // diensten page
 app.get('/diensten', function(req, res) {
-  res.render('pages/diensten');
+  res.render('pages/diensten', {
+    title: 'Lovr - Diensten',
+    page_name: 'diensten'
+});
 });
 
 // contact page
 app.get('/contact', function(req, res){
-  res.render('pages/contact')
+  res.render('pages/contact', {
+    title: 'Lovr - Contact',
+    page_name: 'contact'
+});
 });
 
 // 404 page
 app.get('/*', function(req, res){
-  res.render('pages/404');
+  res.render('pages/404', {
+    title: 'Lovr - 404 Not Found',
+    page_name: '404 Not Found'
+});
 });
 
 app.listen(3000);
